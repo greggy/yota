868169639b08e44f40b74a970e1fa5234f59fc5b
Grigory Fateyev <gfborn@gmail.com>
2015-08-02 19:09:00 +0300

    Change parametrs and place for credintials.

--- README.md
+++ README.md
@@ -7,28 +7,29 @@ Script to change yota tariff or show one (It shows tariff price not speed)
 Get your current tariff:
 
 ```
-> ./yota.py -t
+> ./yota.py -s
 Your tariff is 500
 ```
 
 Сhange current tariff to 300:
 
 ```
-> ./yota.py -s 300
+> ./yota.py -t 300
 ```
 
 Help output:
 
 ```
 > ./yota.py -h
-usage: yota.py [-h] [-s {300,350,400,450,500,550,600,650,700,750,800,850,900}]
-               [-t]
- 
+usage: yota.py [-h] [-t {300,350,400,450,500,550,600,650,700,750,800,850,900}]
+               [-s]
+
 Script to change yota tariff or show one (It shows tariff price not speed)
- 
+
 optional arguments:
   -h, --help            show this help message and exit
-  -s {300,350,400,450,500,550,600,650,700,750,800,850,900}, --speed {300,350,400,450,500,550,600,650,700,750,800,850,900}
-                        Speed to set for yota in rubles
-  -t, --tariff          Show the current tariff in rubles
+  -t {300,350,400,450,500,550,600,650,700,750,800,850,900}, --tariff {300,350,400,450,500,550,600,650,700,750,800,850,900}
+                        Tariff to set for yota in rubles
+  -s, --show            Show the current tariff in rubles
+
 ```
--- yota.py
+++ yota.py
@@ -5,31 +5,25 @@ import ConfigParser
 import argparse
 
 
+LOGIN = ''
+PASSWORD = ''
+
 TARIFF_CODES = {'300':'POS-MA13-0002', '350':'POS-MA13-0003', '400':'POS-MA13-0004', '450':'POS-MA13-0005', '500':'POS-MA13-0006', '550':'POS-MA13-0007', '600':'POS-MA13-0008', '650':'POS-MA13-0009', '700':'POS-MA13-0010', '750':'POS-MA13-0011', '800':'POS-MA13-0012', '850':'POS-MA13-0013', '900':'POS-MA13-0014'}
 AVAILABLE_SPEED = TARIFF_CODES.keys()
 AVAILABLE_SPEED.sort()
 
-def get_settings():
-    "Get login/password from config"
-    cnf = ConfigParser.ConfigParser()
-    cnf.readfp(open('.yota.cnf'))
-    login = cnf.get('general', 'login')
-    password = cnf.get('general', 'password')
-
-    return login, password
 
 def get_args():
     arg_parse = argparse.ArgumentParser(description='Script to change yota tariff or show one (It shows tariff price not speed)')
-    arg_parse.add_argument('-s', '--speed', dest='speed', choices=AVAILABLE_SPEED, type=str, help='Speed to set for yota in rubles')
-    arg_parse.add_argument('-t', '--tariff', dest='tariff', action="store_true", help='Show the current tariff in rubles')
+    arg_parse.add_argument('-t', '--tariff', dest='tariff', choices=AVAILABLE_SPEED, type=str, help='Tariff to set for yota in rubles')
+    arg_parse.add_argument('-s', '--show', dest='show', action="store_true", help='Show the current tariff in rubles')
     args = arg_parse.parse_args()
     return args
 
 def auth_yota(sess):
     auth_url = 'https://login.yota.ru/UI/Login'
-    login, password = get_settings()
 
-    payload = {'IDToken1' : login, 'IDToken2' : password, 
+    payload = {'IDToken1' : LOGIN, 'IDToken2' : PASSWORD, 
                 'goto' : 'https://my.yota.ru/selfcare/loginSuccess', 
                 'gotoOnFail' : 'https://my.yota.ru/selfcare/loginError',
                 'old-token' : '',
@@ -74,7 +68,7 @@ def main():
             offerCode = line.split()[3].split('=')[1]
             offerCode = offerCode.replace('"', '')
 
-    if args.tariff:
+    if args.show:
         if offerCode == '':
             print 'Error. Offer code value is empty'
 
@@ -83,7 +77,7 @@ def main():
         if product == '':
             print 'Error. Product value is empty'
 
-        change_offer(sess, product, args.speed)
+        change_offer(sess, product, args.tariff)
 
 
 
